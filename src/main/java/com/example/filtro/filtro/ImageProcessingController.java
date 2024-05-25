205d597aa0b2ab5e62ae132637e6a2f99069f527
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2024-05-24 23:28:27 -0300

    feat:initiate api providing image filtering application

---
+++ src/main/java/com/example/filtro/filtro/ImageProcessingController.java
@@ -0,0 +1,36 @@
+package com.example.filtro.filtro;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.springframework.http.MediaType;
+import java.io.ByteArrayInputStream;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/images")
+public class ImageProcessingController {
+
+  private final ImageProcessingService imageProcessingService;
+
+  public ImageProcessingController(ImageProcessingService imageProcessingService) {
+    this.imageProcessingService = imageProcessingService;
+  }
+
+  @PostMapping(value = "/process-negative-rgb", produces = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE })
+  public byte[] processNegativeRGB(@RequestBody byte[] imageData) throws IOException {
+
+    BufferedImage image = ImageIO.read(new ByteArrayInputStream(imageData));
+
+    BufferedImage processedImage = imageProcessingService.processNegativeRGB(image);
+
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    ImageIO.write(processedImage, "png", outputStream);
+
+    return outputStream.toByteArray();
+  }
+}
---
+++ src/main/java/com/example/filtro/filtro/ImageProcessingService.java
@@ -0,0 +1,40 @@
+package com.example.filtro.filtro;
+
+import java.awt.Color;
+
+import java.awt.image.BufferedImage;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageProcessingService {
+
+  public String getOriginalImage() {
+
+    return "Imagem original";
+  }
+
+  public String processImage(String imageData) {
+    return "Imagem processada";
+  }
+
+  public BufferedImage processNegativeRGB(BufferedImage image) {
+    int width = image.getWidth();
+    int height = image.getHeight();
+
+    BufferedImage negativeImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+    for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+        Color color = new Color(image.getRGB(x, y));
+        int red = 255 - color.getRed();
+        int green = 255 - color.getGreen();
+        int blue = 255 - color.getBlue();
+        Color negativeColor = new Color(red, green, blue);
+        negativeImage.setRGB(x, y, negativeColor.getRGB());
+      }
+    }
+
+    return negativeImage;
+  }
+}
